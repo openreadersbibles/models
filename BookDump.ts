cb370223d0fb51819dab740c0c83fb43afdcba1e
openreadersbibles <openreadersbibles@gmail.com>
2025-03-25 10:44:18 -0500

    BookDump as class

---
+++ BookDump.ts
@@ -0,0 +1,23 @@
+import { annotationFromObject } from "./Annotation";
+import { UbsBook, Canon } from "./VerseReference";
+
+export class BookDump {
+    readonly book_id: UbsBook;
+    readonly canon: Canon;
+    readonly book_title: string;
+    readonly verses: any[];
+
+    constructor(book_id: UbsBook, canon: Canon, book_title: string, verses: any[]) {
+        this.book_id = book_id;
+        this.canon = canon;
+        this.book_title = book_title;
+
+        /// convert the annotation JSON to objects
+        this.verses = verses.map((row: any) => {
+            if (row.gloss !== null) {
+                row.gloss = annotationFromObject(row.gloss);
+            }
+            return row;
+        });
+    }
+}
--- ProjectDump.ts
+++ ProjectDump.ts
@@ -1,16 +1,9 @@
+import { BookDump } from "./BookDump";
 import { ProjectId } from "./ProjectConfiguration";
-import { Canon, UbsBook } from "./VerseReference";
 
 export interface ProjectDump {
     project_id: ProjectId;
     books: BookDump[];
-}
-
-export interface BookDump {
-    book_id: UbsBook;
-    canon: Canon;
-    book_title: string;
-    verses: any[];
 }
 
 
